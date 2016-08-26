b2bb717c2c467d45fde7cfc1007fa2a8d4ec3e60
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-08-26 20:18:47 +0200

    Add test for FormElement

--- app/components/FormElement/tests/index.test.js
+++ app/components/FormElement/tests/index.test.js
@@ -1,11 +1,32 @@
-// import FormElement from '../index';
+import FormElement from '../index';
 
 import expect from 'expect';
-// import { shallow } from 'enzyme';
-// import React from 'react';
+import { mount } from 'enzyme';
+import React from 'react';
+
+import TextField from 'components/Form/TextField';
+
 
 describe('<FormElement />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  let renderedComponent;
+  beforeEach(() => {
+    renderedComponent = mount(
+      <FormElement
+        label="First name"
+        helpText="Tell us you name"
+        inputComponent={
+          <TextField
+            name="firstName"
+            placeholder="First name"
+            validate="required"
+          />
+        }
+      />
+    );
+  });
+
+  it('should render an error when blurred out with invalid value', () => {
+    renderedComponent.find('input').simulate('blur');
+    expect(renderedComponent.contains('required'));
   });
 });
