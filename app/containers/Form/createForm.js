92e94911d68f6fe52478b2fa824fb6da1c5f0eb5
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-08-29 09:45:04 +0200

    Remove validation logic from Form component

--- app/containers/Form/createForm.js
+++ app/containers/Form/createForm.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { isEmpty, camelCase, trimEnd } from 'lodash';
-import invariant from 'invariant';
 
 import {
   registerForm,
@@ -11,8 +9,6 @@ import {
   change,
 } from './actions';
 import { getFormValues, getFormErrors } from './selectors';
-
-import * as validators from './validators';
 
 
 const generateForm = ({ id }) => (FormComponent) => {
@@ -40,7 +36,6 @@ const generateForm = ({ id }) => (FormComponent) => {
     constructor(props) {
       super(props);
 
-      this.validate = this.validate.bind(this);
       this.attach = this.attach.bind(this);
       this.detach = this.detach.bind(this);
       this.change = this.change.bind(this);
@@ -64,67 +59,6 @@ const generateForm = ({ id }) => (FormComponent) => {
     change(payload) { this.props.change({ ...payload, id }); }
 
     get id() { return id; }
-
-    /**
-     * Parse the validation string passed down from props and return an object
-     * containing data to execute the validator.
-     *
-     * @return {array}
-     * @private
-     */
-    parseValidators(validationString) {
-      // Parse validators, remove duplicates, include default validators
-      const strValidators = Object.keys(
-        trimEnd(validationString, '|').split('|')
-          .reduce((acc, el) => (el ? { ...acc, [el]: null } : acc), {})
-      );
-      return strValidators.map((strValidator) => {
-        let validator = strValidator;
-        let params = [];
-
-        // Parameters are separated from the validation with a colon
-        if (strValidator.indexOf(':') > 0) {
-          [validator, ...params] = strValidator.split(':');
-
-          // Multiple parameters may be delimited by a comma
-          if (params[0].indexOf(',') > -1) {
-            params = params[0].split(',');
-          }
-        }
-        validator = camelCase(validator);
-        // Replace any empty string parameters with undefined
-        params = params.map((el) => (isEmpty(el) ? undefined : el));
-
-        return { validator, params };
-      });
-    }
-
-    /**
-     * Validate the component data with its validation string and set form state
-     * appropriately.
-     *
-     * @return {void}
-     * @private
-     */
-    validate(component) {
-      const { validate, defaultValidations } = component.props;
-      const errors = [];
-
-      const validatorsParams = this.parseValidators(`${validate}|${defaultValidations}`);
-      validatorsParams.forEach(({ validator, params }) => {
-        // Confirm that the validator is recognized
-        invariant(
-          validator in validators,
-          `${validator} validator is not recognized in validators.js`
-        );
-
-        if (!validators[validator](component.getValue(), params, this.data)) {
-          errors.push(validator);
-        }
-      });
-
-      return errors;
-    }
 
     render() {
       return React.Children.only(
