981000b531d651979ab534cebd1f58b933eda6a2
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-09-01 13:09:20 +0200

    Fix createFormSubmitAction docstring

--- app/containers/Form/createFormActions.js
+++ app/containers/Form/createFormActions.js
@@ -6,17 +6,16 @@ import { SUBMIT_REQUEST } from './constants';
  * to atain communication between different sagas.
  *
  * @param  {function} requestAction
- *          An action creator to trigger the request action.
+ *         An action creator to trigger the request action.
  * @param  {array} types
- *          An array containing two constants, the first should be the success
- *          and the second the failure constants to the request.
+ *         An array containing two constants, the first should be the success
+ *         and the second the failure constants to the request.
  *
  * @return {function}
- *          Returns a function that accepts the dispatch method. When this
- *          function is bound with dispatch, it returns another function that
- *          acceptsthe form id and the form data.
- *          This function then returns a promise which the Form saga can
- *          understand.
+ *         Returns a function that accepts the form id, the form values and the
+ *         dispatch method.
+ *         This function then returns a promise which the Form saga can
+ *         understand.
  */
 const createFormSubmitAction = (requestAction, types) => {
   const [successActionType, failureActionType] = types;
